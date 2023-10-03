ba5ed6761b3c68001185771e7fb7ba3a48a415ac
TangoIndiaMango <aliyutimileyin2340@gmail.com>
2023-10-03 18:14:07 -0500

    updated to required format

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,9 +1,20 @@
-export default function updateStudentGradeByCity(arr, city, newGrades) {
-  return arr
-    .filter(student => student.location === city)
-    .map(student => {
-      let grade = newGrades.find(g => g.studentId === student.id);
-      return { ...student, grade: (grade && grade.grade) || 'N/A' };
-    });
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  // Use the filter() method to create a new array containing only students from the specified city.
+  const studentsInCity = students.filter(student => student.location === city);
+
+  // Use the map() method to iterate over the students in the city and update their grades.
+  const updatedStudents = studentsInCity.map(student => {
+    // Find the corresponding grade object in newGrades based on the student's id.
+    const grade = newGrades.find(g => g.studentId === student.id);
+
+    // If a grade is found, update the student's grade with the new grade; otherwise, set it to 'N/A'.
+    const updatedGrade = (grade && grade.grade) || 'N/A';
+
+    // Create a new object with the updated grade and all other student properties.
+    return { ...student, grade: updatedGrade };
+  });
+
+  // Return the array of updated students.
+  return updatedStudents;
 }
 
