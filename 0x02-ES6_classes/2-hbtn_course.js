a79d86189f9de40dcd9dc132dc2d5c35b3f01cc5
TangoIndiaMango <aliyutimileyin2340@gmail.com>
2023-10-03 17:20:15 -0500

    add constraints

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,13 +1,13 @@
-class HolbertonCourse {
+export default class HolbertonCourse {
 	constructor(name, length, students) {
 		if (typeof name != "string") {
 			throw new TypeError("Name must be a string");
 		}
 		if (typeof length != "number") {
-			throw new TypeError("Length must be of type number")
+			throw new TypeError("Length must be of type number");
 		}
 		if (!Array.isArray(students) || !students.every((student) => typeof student === "string")) {
-			throe new TypeError("Student must be an array of strings");
+			throw new TypeError("Student must be an array of strings");
 		}
 
 		this._name = name;
@@ -21,14 +21,20 @@ class HolbertonCourse {
 	}
 
 	set name(value) {
+		if (typeof name != "string"){
+			throw new TypeError("Name must be a string");
+		}
 		this._name = value;
 	}
 
 	get length() {
-		return this._length
+		return this._length;
 	}
 
 	set length(value) {
+		if (typeof length != "number") {
+                        throw new TypeError("Length must be of type number");
+		}
 		this._length = value;
 	}
 
@@ -37,6 +43,9 @@ class HolbertonCourse {
 	}
 
 	set students(value) {
+		if (!Array.isArray(students) || !students.every((student) => typeof student === "string")) {
+                        throw new TypeError("Student must be an array of strings");
+		}
 		this._students = value;
 	}
 }
