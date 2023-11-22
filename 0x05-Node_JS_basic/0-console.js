3297dbab4ea166666d05e2cc7ada2c6818fadcf0
TangoIndiaMango <aliyutimileyin2340@gmail.com>
2023-11-22 00:56:06 -0600

    this commit will push node js basics

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Display messages on the STDOUT.
+ * @param {String} msg message to display.
+ * @author Timilehin Aliyu <https://github.com/TangoIndiaMango>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
